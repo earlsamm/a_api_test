278c243c4999dba4776fad44e9be91af536e146f
Onowuko Samuel <39769754+earlsamm@users.noreply.github.com>
2023-03-05 03:27:23 +0100

    Adding another endpoint

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
-require "./vendor/autoload.php";
+require './vendor/autoload.php';
 
 set_error_handler('ErrorHandler::handleError');
 set_exception_handler('ErrorHandler::handleException');
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname("./"));
+$dotenv = Dotenv\Dotenv::createImmutable(dirname('./'));
 $dotenv->load();
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -18,18 +18,17 @@ $resource = $parts[2];
 
 $id = $parts[3] ?? null;
 
-
 if ($resource != 'tasks') {
     http_response_code(404);
     // header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
     exit;
 }
 
-header("Content-Type: application/json; charset= UTF-8");
+header('Content-Type: application/json; charset= UTF-8');
 
 // require "./src/TaskController.php";
 
-$database = new Database($_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
+$database = new Database($_ENV['DB_HOST'], $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 
 $task_gateway = new TaskGateway($database);
 
@@ -37,3 +36,5 @@ $controller = new TaskController($task_gateway);
 
 $controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
 
+//Now we add another route
+$routes = 'sammy';
